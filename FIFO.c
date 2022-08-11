0daf3c3ef9c3f5be5db0bd3d165eb7ace19bd7ef
TNThung <50406653+tnthung@users.noreply.github.com>
2022-08-11 16:45:08 +0800

    Add files via upload

---
+++ FIFO.c
@@ -0,0 +1,59 @@
+#include <stdlib.h>
+#include <memory.h>
+#include "FIFO.h"
+
+
+void newFIFI(FIFO *fifo, int len, int size)
+{
+  if (fifo->init) return;
+
+  fifo->init = 1;
+  fifo->nd   = 0;
+  fifo->ne   = 0;
+  fifo->cnt  = 0;
+  fifo->sz   = size;
+  fifo->len  = len;
+
+  fifo->_ = malloc(len * size);
+}
+
+void delFIFO(FIFO *fifo)
+{
+  if (!fifo->_) return;
+
+  fifo->init = 0;
+  free(fifo->_);
+}
+
+int enqFIFO(FIFO *fifo, void *src)
+{
+  if (!fifo->init)            return -1;
+  if (fifo->cnt >= fifo->len) return -1;
+
+  memcpy(&(fifo->_[fifo->ne]), src, fifo->sz);
+
+  fifo->ne = ++(fifo->ne) % fifo->len;
+  fifo->cnt++;
+}
+
+void *deqFIFO(FIFO *fifo)
+{
+  int i = fifo->nd;
+
+  if (!fifo->init)    return -1;
+  if (fifo->cnt == 0) return -1;
+
+  fifo->ne = ++(fifo->ne) % fifo->len;
+  fifo->cnt--;
+
+  return (fifo->_ + i*fifo->sz);
+}
+
+void *seeFIFO(FIFO *fifo)
+{
+  if (!fifo->init)    return -1;
+  if (fifo->cnt == 0) return -1;
+
+  return (fifo->_ + fifo->nd*fifo->sz);
+}
+
---
+++ FIFO.h
@@ -0,0 +1,22 @@
+
+
+typedef struct FIFO
+{
+  void *_;
+
+  unsigned int ne;    // next enq
+  unsigned int nd;    // next deq
+  unsigned int sz;    // size of each element
+  unsigned int len;   // length of FIFO
+  unsigned int cnt;   // count of items in FIFO
+  unsigned int init;  // if FIFO initialized
+
+} FIFO;
+
+
+void newFIFI(FIFO *fifo, int len, int size);
+void delFIFO(FIFO *fifo);
+
+int   enqFIFO(FIFO *fifo, void *src); // push element into FIFO
+void *deqFIFO(FIFO *fifo);            // pop element from FIFO
+void *seeFIFO(FIFO *fifo);            // get element without removing
